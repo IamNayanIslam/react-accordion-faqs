978fd710d1fb36b7a889bdbfdb46500826f13154
IamNayanIslam <iamnayanislam@gmail.com>
2023-12-17 16:34:10 +0600

    interactivity and responsiveness

--- src/Components/AccordionItem/AccordionItem.jsx
+++ src/Components/AccordionItem/AccordionItem.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function AccordionItem({ faq }) {
+  const [isOpen, setIsOpen] = useState(false);
+  function toggleIsOpen() {
+    setIsOpen(!isOpen);
+  }
   return (
     <>
-      <div className="item">
-        <p className="number">0{faq.id}</p>
+      <div className={`item ${isOpen ? "open" : ""}`}>
+        <p className="number">{faq.id < 10 ? `0${faq.id}` : faq.id}</p>
         <p className="text">{faq.title}</p>
-        <p className="icon">-</p>
-        <div className="content-box">{faq.description}</div>
+        <p className="icon" onClick={toggleIsOpen}>
+          {isOpen ? "-" : "+"}
+        </p>
+        {isOpen && <div className="content-box">{faq.description}</div>}
       </div>
     </>
   );
